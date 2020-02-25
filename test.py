e92d310fcc334509fb46819ea988853f7a35c919
Trung Nguyen <Trungn1991@gmail.com>
2020-02-25 12:19:00 -0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ MONGO_HOST = "10.1.0.90"
 MONGO_PORT = 25
 MONGO_DB = "db_name"
 MONGO_USER = "username"
-MONGO_PASS = "password"
+MONGO_PASS = "fee3fa69-d0db-4efe-8371-3d24d65cb426"
 
 con = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
 db = con[MONGO_DB]
